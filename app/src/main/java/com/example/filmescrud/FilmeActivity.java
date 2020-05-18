4775584fb3c60b95eb7b4852223bc33dc06d2b47
LUIS GUILHERME SOARES MORAES <luis.moraes37@etec.sp.gov.br>
2020-05-18 19:01:47 -0300

    first

---
+++ app/src/main/java/com/example/filmescrud/FilmeActivity.java
@@ -0,0 +1,15 @@
+package com.example.filmescrud;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FilmeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.filme_activity);
+    }
+}
+
